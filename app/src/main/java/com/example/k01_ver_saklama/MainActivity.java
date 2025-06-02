f972a622ad142083862cb7fff00717baa0a102ad
Zahid İyigökler <iyigoklerzahid55@gmail.com>
2025-06-02 09:57:43 +0300

    Bugs were fixed and codes were simplified.

--- app/src/main/java/com/example/k01_ver_saklama/MainActivity.java
+++ app/src/main/java/com/example/k01_ver_saklama/MainActivity.java
@@ -1,49 +1,54 @@
 package com.example.k01_ver_saklama;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     EditText editText;
     TextView textView;
+    SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
         editText = findViewById(R.id.editText);
         textView = findViewById(R.id.textView);
-
-
-        SharedPreferences sharedPreferences = this.getSharedPreferences("com.example.k01_ver_saklama", Context.MODE_PRIVATE);
-
+        sharedPreferences = this.getSharedPreferences("com.example.k01_ver_saklama",Context.MODE_PRIVATE);
+        int storedAge = sharedPreferences.getInt("storedAge",0);
+        if (storedAge == 0) {
+            textView.setText("Your age : ");
+        } else {
+            textView.setText("Your age : "+ storedAge);
+        }
         // Todo: Uygulamaya ikon eklenebilir.
         // Todo: Açıklayıcı metin ekle.
         // Todo: Readme tanıtım ve açıklama metni yaz.
-
+        // Todo: editText'e sayı dışı verilere hata mesajı verdir.
 
 
     }
 
-
-    @SuppressLint("CommitPrefEdits")
     public void buton(View view) {
         if (!editText.getText().toString().matches("")) {
             int userAge = Integer.parseInt(editText.getText().toString());
             textView.setText("Your age : " + userAge);
 
-            SharedPreferences.edit().putInt("storedAge",userAge).apply();
+            sharedPreferences.edit().putInt("storedAge",userAge).apply();;
 
+        }
+    }
 
+    public void delete(View view) {
+        int storedData = sharedPreferences.getInt("storedAge",0);
+        if (storedData != 0) {
+            sharedPreferences.edit().remove("storedAge").apply();
+            textView.setText("Your age : ");
         }
     }
 }
